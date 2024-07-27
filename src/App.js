4dcb6304ab82781ba4a788cdc2c406b32029f08a
Andrea <anfrescortes@gmail.com>
2024-07-27 13:46:26 +0200

    App is showing in the browser into main branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
+  // const prayer = [
+  //   "Padre nuestro que estás en el cielo,santificado sea tu nombre; venga a nosotros tu reino; hágase tu voluntad, en la tierra como en el cielo.Danos hoy nuestro pan de cada día; perdona nuestras ofensas como también nosotros perdonamos a los que nos ofenden; no nos dejes caer en la tentación, y líbranos del mal. Amén",
+  // ];
 
-  
+  // // console.log("el padre nuestro", prayer);
+
+  // function dividePerPhrase(parrgraph) {
+  //   // const characters = /([.!?,;])s*(?=[A-Z]|$)/g;
+  //   // Just need to add the regex for all special characters
+  //   let phrases = parrgraph.map((phrase) => phrase.split(/[;,.]/));
+
+  //   return phrases;
+  // }
+
+  // let dividedPrayer = dividePerPhrase(prayer);
+
+  // console.log(dividedPrayer);
+  // //at this point dividePerPhrase is an array with 1 array inside
+
+  // function saveFromDevil(dividedPrayer) {
+  //   let singularPhrase = dividedPrayer[0];
+  //   let reversedPrayer = [];
+  //   let result = 0;
+
+  //   for (let i = 0; i < singularPhrase.length; i++) {
+  //     reversedPrayer.unshift(singularPhrase[i]);
+  //     result++;
+  //   }
+  //   return reversedPrayer;
+  // }
+  // let reversed = saveFromDevil(dividedPrayer);
+
+  // console.log(
+  //   "El padre nuestro en frases",
+  //   dividePerPhrase(prayer),
+  //   "padre nuestro al reves",
+  //   reversed
+  // );
+
+  return (
+    <div className="title">
+      <header className="App-header">
+        <p>Hola</p>
+      </header>
     </div>
   );
 }
